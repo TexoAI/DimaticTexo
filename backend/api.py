52a0c0e9d0f84b65443f4feda3053cec6702fb79
Quackvin <kevin.kuek.95@gmail.com>
2025-08-17 21:41:05 +0800

    add vercel to CORS

--- backend/api.py
+++ backend/api.py
@@ -131,16 +131,16 @@ async def log_requests_middleware(request: Request, call_next):
         raise
 
 # Define allowed origins based on environment
-allowed_origins = [os.getenv("NEXT_PUBLIC_URL")]
+allowed_origins = [os.getenv("NEXT_PUBLIC_URL"), 'https://dimatic-texo-mg8o.vercel.app/']
 allow_origin_regex = None
 
 # Add staging-specific origins
 if config.ENV_MODE == EnvMode.LOCAL:
-    allowed_origins.append(os.getenv("NEXT_PUBLIC_URL"))
+    allowed_origins.append([os.getenv("NEXT_PUBLIC_URL"), 'https://dimatic-texo-mg8o.vercel.app/'])
 
 # Add staging-specific origins
 if config.ENV_MODE == EnvMode.STAGING:
-    allowed_origins.append(os.getenv("NEXT_PUBLIC_URL"))
+    allowed_origins.append([os.getenv("NEXT_PUBLIC_URL"), 'https://dimatic-texo-mg8o.vercel.app/'])
     allow_origin_regex = r"https://suna-.*-prjcts\.vercel\.app"
 
 app.add_middleware(
