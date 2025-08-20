a9cfc707be1501f4259450767d4ee996930168bc
mantalope1995 <liam.h995@yahoo.com>
2025-08-20 12:02:01 +0800

    delete

--- frontend/src/components/agents/config/index.ts
+++
@@ -1,5 +0,0 @@
-export { AgentHeader } from './agent-header';
-export { VersionAlert } from './version-alert';
-export { AgentBuilderTab } from './agent-builder-tab';
-export { ConfigurationTab } from './configuration-tab';
-export { ProfilePictureDialog } from './profile-picture-dialog'; 
